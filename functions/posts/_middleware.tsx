a13ce07b9db6f321a3560a721bfba39c6d2834cc
chilitreat <dendenden00@gmail.com>
2025-08-10 02:03:31 +0900

    OGP修正

--- functions/posts/_middleware.tsx
+++ functions/posts/_middleware.tsx
@@ -1,30 +1,42 @@
-import vercelOGPagesPlugin from "@cloudflare/pages-plugin-vercel-og";
+import vercelOGPagesPlugin from '@cloudflare/pages-plugin-vercel-og';
 
-export const onRequest = vercelOGPagesPlugin({
-  imagePathSuffix: "/og-image.png", // ファイル名を定義
-  component: ({ pathname }: { pathname: string }) => {
+interface Props {
+  ogTitle: string;
+}
+
+export const onRequest = vercelOGPagesPlugin<Props>({
+  imagePathSuffix: '/og-image.png', // ファイル名を定義
+  component: ({ ogTitle, pathname }: { pathname: string }) => {
     // pathnameから最後の要素を抜き出す
-    const paths = pathname.split("/");
+    const paths = pathname.split('/');
     const slug = paths[paths.length - 1];
 
     return (
       <div
         style={{
           // flexboxで中央寄せ
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
           // 画像の幅いっぱいまでdivを広げる
-          width: "100%",
-          height: "100%",
+          width: '100%',
+          height: '100%',
         }}
       >
-        <h1 style={{ fontSize: "80px" }}>
-          This is {slug} article
-        </h1>
+        <h1 style={{ fontSize: '80px' }}>{ogTitle}</h1>
+        <h2 style={{ fontSize: '30px' }}>{slug}</h2>
       </div>
     );
+  },
+  extractors: {
+    on: {
+      title: (props) => ({
+        element(element) {
+          props.ogTitle = element.getAttribute('content');
+        },
+      }),
+    },
   },
   options: {
     // 画像のサイズを指定
